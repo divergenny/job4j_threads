ca1098e6dcfaeb5a0ac563b06bd168bbc2e520b0
divergenny <divergenny@gmail.com>
2025-07-13 20:49:47 +0500

    2. Создание и запуск нити

---
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -0,0 +1,20 @@
+package ru.job4j.concurrent;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread another = new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println(Thread.currentThread().getName());
+                    }
+                }
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        another.start();
+        second.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
